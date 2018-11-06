3951bfa48149418ac02744161cc561a88db78838
Shane Hofstetter <shane.hofstetter@gmail.com>
2018-11-06 01:46:27 +0100

    add command to create work item

--- src/youtrack-workitem.ts
+++ src/youtrack-workitem.ts
@@ -4,6 +4,7 @@ import {printObject, RawPrinter, toDateString} from "./utils/printer";
 import {WorkItem} from "youtrack-rest-client/dist/entities/workItem";
 import {handleError} from "./utils/errorHandler";
 import {formatDuration} from "./utils/formatters/durationFormatter";
+import {CreateWorkItemCommand} from "./commands/createWorkItemCommand";
 
 program
     .command('list <issueId>')
@@ -28,6 +29,7 @@ program
                     return formattedWorkItem;
                 });
 
+                // TODO: sort work-items by date
                 printObject(formattedWorkItems, {
                     columnConfig: {
                         0: {
@@ -44,6 +46,15 @@ program
                 });
             }).catch(handleError);
         });
+    });
+
+program
+    .command('create')
+    .description('create new work item for an issue (opens prompt)')
+    .alias('c')
+    .option('-r, --raw', 'print raw json')
+    .action((args) => {
+        return new CreateWorkItemCommand().execute(args.raw);
     });
 
 startCommander();
