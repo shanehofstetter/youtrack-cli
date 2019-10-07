797f0ff3162f9350dc4c7aa2cc2aee2322bf46f7
IJustDev <alexanderpanovi@yahoo.de>
2019-10-07 17:04:37 +0200

    Updated CreateIssueCommand
    Changed the type of project from input to list so that the user is able
    to select one of the suggested projects

--- src/commands/issue/createIssueCommand.ts
+++ src/commands/issue/createIssueCommand.ts
@@ -7,55 +7,55 @@ import * as inquirer from "inquirer";
 
 export class CreateIssueCommand implements YoutrackCliCommand {
 
-    private readonly issueQuestions = [
-        {
-            type: 'input',
-            name: 'project',
-            message: 'ProjectId: ',
-            validate: (projectId: any) => {
-                if (projectId.length >= 1) {
-                    return true;
-                }
-                return chalk.red('please provide a valid projectId');
+    private getIssuePrompt(client: YoutrackClient){
+        return [
+            {
+                type: 'list',
+                name: 'project',
+                message: 'Project:',
+                choices: () => {
+                    return client.projects.all().then((projects) => {
+                        return projects.map((proj) => proj.shortName) as string[];
+                    });
+                },
             },
-        },
-        {
-            type: 'input',
-            name: 'summary',
-            message: 'Summary (Title):',
-            validate: (summary: any) => {
-                if (summary.length >= 1) {
-                    return true;
-                }
-                return chalk.red('please provide a valid summary');
+            {
+                type: 'input',
+                name: 'summary',
+                message: 'Summary (Title):',
+                validate: (summary: any) => {
+                    if (summary.length >= 1) {
+                        return true;
+                    }
+                    return chalk.red('please provide a valid summary');
+                },
             },
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Description:',
-        },
-        {
-            type: 'input',
-            name: 'assignee',
-            message: 'Assignee:',
-        },
-        {
-            type: 'input',
-            name: 'type',
-            message: 'Type:',
-            validate: (type: string) => {
-                if (type.length >= 1) {
-                    return true;
-                }
-                return chalk.red('please provide a valid type');
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Description:',
             },
-        }
-    ];
+            {
+                type: 'input',
+                name: 'assignee',
+                message: 'Assignee:',
+                validate: (assignee: string) => {
+                    if (assignee.length === 0) {
+                        return true;
+                    }
+                    return client.users.byName(assignee).then(() => {
+                        return true;
+                    }).catch(() => {
+                        return chalk.red('please provide a valid assignee or leave blank for unassigned');
+                    });
+                },
+            },
+        ];
+    }
 
     execute() {
         return actionWrapper((client: YoutrackClient) => {
-            return inquirer.prompt(this.issueQuestions).then((answers: any) => {
+            return inquirer.prompt(this.getIssuePrompt(client)).then((answers: any) => {
                 const issueObj = {
                     summary: answers.summary,
                     description: answers.description,
@@ -63,7 +63,7 @@ export class CreateIssueCommand implements YoutrackCliCommand {
                     type: answers.type,
                     assignee: undefined,
                 };
-                if (answers.assignee === '') {
+                if (answers.assignee !== '') {
                     issueObj.assignee = answers.assignee;                    
                 }
                 client.issues.create(issueObj).then((issueId) => {
